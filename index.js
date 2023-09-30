b016d9239aca6389049f891ee9d0726befbf512b
Josie Cisneros <josiecisneros1224@gmail.com>
2023-09-30 09:16:19 -0600

    unfinished lab

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = array => array.map(sentence => sentence.split(" ").map( (word) => {
+  return word[0].toUpperCase() + word.substring(1)
+}).join(" "))
+  
+console.log(titleCased)
+
+
+
+
+
+
